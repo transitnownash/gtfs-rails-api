d18a0f27a979b24914d5f4b300fabced1da1d17b
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-07-03 22:01:36 -0500

    Adjust timezone setting

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module GtfsRailsApi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = ENV['GTFS_TIMEZONE'] || 'America/New_York'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
