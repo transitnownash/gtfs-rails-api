ee355f6d1495a5bd83e5c91b0d66e214fd9f9a3b
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-08-04 00:18:33 -0500

    Sort results properly

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -8,11 +8,11 @@ class StopsController < ApplicationController
     render json: paginate_results(Stop.all)
   end
 
-  # GET /stops/near/36.166512,86.781581
-  # GET /stops/near/36.166512,86.781581/100
+  # GET /stops/near/36.165,-86.78406
+  # GET /stops/near/36.165,-86.78406/100
   def nearby
     radius = params[:radius] || 100
-    render json: paginate_results(Stop.within(radius, origin: [params[:latitude], params[:longitude]]))
+    render json: paginate_results(Stop.within(radius, origin: [params[:latitude], params[:longitude]]).by_distance(origin: [params[:latitude], params[:longitude]]))
   end
 
   # GET /stops/1
