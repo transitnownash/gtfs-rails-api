da6c5d77cce09a13510d89d479c63cf223eb6c7c
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-04-15 20:43:14 -0500

    Update config

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require 'opentelemetry/sdk'
 require 'opentelemetry/exporter/otlp'
 require 'opentelemetry/instrumentation/all'
 
 OpenTelemetry::SDK.configure do |c|
-    c.use_all() # enables all instrumentation!
+  c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
+  c.use 'OpenTelemetry::Instrumentation::Rack'
+  c.use 'OpenTelemetry::Instrumentation::ActionPack'
+  c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+  c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+  c.use 'OpenTelemetry::Instrumentation::ActionView'
+  c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
+  c.use 'OpenTelemetry::Instrumentation::Mysql2'
+  c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
+  c.use 'OpenTelemetry::Instrumentation::Rails'
 end
