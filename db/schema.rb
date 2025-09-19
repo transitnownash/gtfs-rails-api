c650830b52e646428c857f07935279031fbf35d9
Stephen Yeargin <stephen@yearg.in>
2025-09-19 17:24:02 -0500

    Bump schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_14_184344) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_14_184344) do
   create_table "agencies", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "agency_gid"
     t.string "agency_name", null: false
